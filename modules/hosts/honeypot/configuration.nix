1250840a86d80d1cd91475ea3283b9e0c2dd18b0
Kevin Schoonover <github@kschoon.me>
2026-05-06 09:28:56 -0700

    latest from honeypot

--- modules/hosts/honeypot/configuration.nix
+++ modules/hosts/honeypot/configuration.nix
@@ -1,60 +1,66 @@
 { inputs, self, ... }:
 {
-  flake.nixosModules.honeypotConfiguration = { config, pkgs, lib, ... }: {
-    imports = [
-      inputs.nixos-hardware.nixosModules.dell-xps-13-9380
-      self.nixosModules.honeypotHardware
+  flake.nixosModules.honeypotConfiguration =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      imports = [
+        inputs.nixos-hardware.nixosModules.dell-xps-13-9380
+        self.nixosModules.honeypotHardware
 
-      # System fundamentals
-      self.nixosModules.boot
-      self.nixosModules.nix-settings
-      self.nixosModules.networking
-      self.nixosModules.locale
-      self.nixosModules.audio
-      self.nixosModules.user
-      self.nixosModules.shell
-      self.nixosModules.certificates
+        # System fundamentals
+        self.nixosModules.boot
+        self.nixosModules.nix-settings
+        self.nixosModules.networking
+        self.nixosModules.locale
+        self.nixosModules.audio
+        self.nixosModules.user
+        self.nixosModules.shell
+        self.nixosModules.certificates
 
-      # Desktop
-      self.nixosModules.niri
+        # Desktop
+        self.nixosModules.niri
 
-      self.nixosModules.dotfiles
+        self.nixosModules.dotfiles
 
+        # Applications
+        self.nixosModules.browser
+        self.nixosModules.communication
+        self.nixosModules.spotify
+        self.nixosModules.kitty
+        self.nixosModules.neovim
+        self.nixosModules.git
+        self.nixosModules.tmux
 
-      # Applications
-      self.nixosModules.browser
-      self.nixosModules.communication
-      self.nixosModules.spotify
-      self.nixosModules.kitty
-      self.nixosModules.neovim
-      self.nixosModules.git
-      self.nixosModules.tmux
+        # Languages
+        self.nixosModules.go
+        self.nixosModules.rust
+        self.nixosModules.python
+        self.nixosModules.node
+        self.nixosModules.lua
+        self.nixosModules.nix-lang
+        self.nixosModules.bash-lang
+        self.nixosModules.elixir
+        self.nixosModules.c-cpp
+        self.nixosModules.misc-lang
 
-      # Languages
-      self.nixosModules.go
-      self.nixosModules.rust
-      self.nixosModules.python
-      self.nixosModules.node
-      self.nixosModules.lua
-      self.nixosModules.nix-lang
-      self.nixosModules.bash-lang
-      self.nixosModules.elixir
-      self.nixosModules.c-cpp
-      self.nixosModules.misc-lang
+        # Infrastructure & services
+        self.nixosModules.infrastructure
+        self.nixosModules.docker
+        self.nixosModules.security
+        self.nixosModules.services
+        self.nixosModules.bluetooth
+        self.nixosModules.vpn
+        self.nixosModules.gaming
+        self.nixosModules.mobile
+        self.nixosModules.cli-utils
+      ];
 
-      # Infrastructure & services
-      self.nixosModules.infrastructure
-      self.nixosModules.docker
-      self.nixosModules.security
-      self.nixosModules.services
-      self.nixosModules.bluetooth
-      self.nixosModules.vpn
-      self.nixosModules.gaming
-      self.nixosModules.mobile
-      self.nixosModules.cli-utils
-    ];
-
-    networking.hostName = "honeypot";
-    system.stateVersion = "23.05";
-  };
+      networking.hostName = "honeypot";
+      system.stateVersion = "25.11";
+    };
 }
